47f8e5e937eb03c2aa60da65578a9e8d8dc11d24
AbdurrabKhan198 <abdurrabkhan709@gmail.com>
2025-07-14 22:28:43 +0530

    forms

--- phase-3/app.py
+++ phase-3/app.py
@@ -1,13 +1,47 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,url_for, redirect,flash
+from forms import RegistrationForm
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 
-@app.route("/feedback", methods=["GET", "POST"])
-def feedback():
-    if request.method == "POST":
-        name = request.form.get("name")
-        message = request.form.get("message")
+# @app.route("/feedback", methods=["GET", "POST"])
+# def feedback():
+#     if request.method == "POST":
+#         name = request.form.get("name")
+#         message = request.form.get("message")
 
-        return render_template("thankyou.html", name=name, message=message)
+#         return render_template("thankyou.html", name=name, message=message)
 
-    return render_template("feedback.html")
+#     return render_template("feedback.html")
+
+# @app.route("/", methods=["GET", "POST"])
+# def form():
+#     if request.method == "POST":
+#         name = request.form.get("name")
+#         message = request.form.get("message")
+#         if not name:
+#             flash("Name can not be empty", "error")
+#             return redirect(url_for("form"))
+#         flash(f"THanks {name} for your feedback {message}" , "success")
+#         return redirect(url_for("thankyou"))
+
+#     return render_template("form.html")
+
+# @app.route("/thankyou",methods=["GET"])
+# def thankyou():
+#         message = request.args.get("message")
+#         return render_template("thankyou.html",message=message)
+        
+
+@app.route("/", methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        flash(f"Welcome , {name}! Your registered sucessfully","success")
+    return render_template("register.html", form=form)
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
